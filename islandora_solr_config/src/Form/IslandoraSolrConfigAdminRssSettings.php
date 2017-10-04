321484d78a1f5d38ba54b0310976dbf74c060812
willtp87 <willtp87@hotmail.com>
2017-10-04 20:15:40 -0300

    solr config admins working

--- islandora_solr_config/src/Form/IslandoraSolrConfigAdminRssSettings.php
+++ islandora_solr_config/src/Form/IslandoraSolrConfigAdminRssSettings.php
@@ -1,15 +1,9 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\islandora_solr_config\Form\IslandoraSolrConfigAdminRssSettings.
- */
-
 namespace Drupal\islandora_solr_config\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element;
 
 /**
  * Admin settings for the RSS exposed by Solr.
@@ -23,128 +17,105 @@ class IslandoraSolrConfigAdminRssSettings extends FormBase {
     return 'islandora_solr_config_admin_rss_settings';
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state) {
     // Get variables.
-  // @FIXME
-// Could not extract the default value because it is either indeterminate, or
-// not scalar. You'll need to provide a default value in
-// config/install/islandora_solr_config.settings.yml and config/schema/islandora_solr_config.schema.yml.
     $rss_item = \Drupal::config('islandora_solr_config.settings')->get('islandora_solr_config_rss_item');
-    // @FIXME
-    // Could not extract the default value because it is either indeterminate, or
-    // not scalar. You'll need to provide a default value in
-    // config/install/islandora_solr_config.settings.yml and config/schema/islandora_solr_config.schema.yml.
     $rss_channel = \Drupal::config('islandora_solr_config.settings')->get('islandora_solr_config_rss_channel');
 
-    $form = ['#tree' => TRUE];
+    $form['#tree'] = TRUE;
 
     $form['rss_item'] = [
       '#type' => 'fieldset',
-      '#title' => t('Item settings'),
+      '#title' => $this->t('Item settings'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
-      '#description' => t('If the Solr Search Results Display fields are limited, only those fields which are configured for display can be used here, if an un-configured field is used it will be ignored. To take full control over the RSS item output you can also override the following method: IslandoraSolrResultsRSS::rssItem()'),
+      '#description' => $this->t('If the Solr Search Results Display fields are limited, only those fields which are configured for display can be used here, if an un-configured field is used it will be ignored. To take full control over the RSS item output you can also override the following method: IslandoraSolrResultsRSS::rssItem()'),
     ];
     $form['rss_item']['title'] = [
       '#type' => 'textfield',
-      '#title' => t('Title'),
-      '#description' => t('Solr field to populate the item title.'),
+      '#title' => $this->t('Title'),
+      '#description' => $this->t('Solr field to populate the item title.'),
       '#autocomplete_path' => 'islandora_solr/autocomplete_luke',
       '#default_value' => $rss_item['title'],
       '#required' => TRUE,
     ];
     $form['rss_item']['description'] = [
       '#type' => 'textfield',
-      '#title' => t('Description'),
+      '#title' => $this->t('Description'),
       '#description' => t('Solr field to populate the item synopsis.'),
       '#autocomplete_path' => 'islandora_solr/autocomplete_luke',
       '#default_value' => $rss_item['description'],
     ];
     $form['rss_item']['author'] = [
       '#type' => 'textfield',
-      '#title' => t('Author'),
-      '#description' => t('Solr field to populate the item author.'),
+      '#title' => $this->t('Author'),
+      '#description' => $this->t('Solr field to populate the item author.'),
       '#autocomplete_path' => 'islandora_solr/autocomplete_luke',
       '#default_value' => $rss_item['author'],
     ];
     $form['rss_item']['category'] = [
       '#type' => 'textfield',
-      '#title' => t('Category'),
-      '#description' => t('Solr field to populate the item category.'),
+      '#title' => $this->t('Category'),
+      '#description' => $this->t('Solr field to populate the item category.'),
       '#autocomplete_path' => 'islandora_solr/autocomplete_luke',
       '#default_value' => $rss_item['category'],
     ];
     $form['rss_item']['pubDate'] = [
       '#type' => 'textfield',
-      '#title' => t('Publication date'),
-      '#description' => t('Solr field to populate the item publication date (pubDate).'),
+      '#title' => $this->t('Publication date'),
+      '#description' => $this->t('Solr field to populate the item publication date (pubDate).'),
       '#autocomplete_path' => 'islandora_solr/autocomplete_luke',
       '#default_value' => $rss_item['pubDate'],
     ];
     $form['rss_item']['enclosure_dsid'] = [
       '#type' => 'textfield',
-      '#title' => t('Enclosure (datastream ID)'),
-      '#description' => t('Fill out a datastream ID to be added as a media enclosure. Defaults to thumbnail (TN).'),
+      '#title' => $this->t('Enclosure (datastream ID)'),
+      '#description' => $this->t('Fill out a datastream ID to be added as a media enclosure. Defaults to thumbnail (TN).'),
       '#default_value' => $rss_item['enclosure_dsid'],
     ];
     $form['rss_channel'] = [
       '#type' => 'fieldset',
-      '#title' => t('Channel settings'),
+      '#title' => $this->t('Channel settings'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
       '#description' => t('To take full control over the RSS channel output you can also override the following method: IslandoraSolrResultsRSS::rssChannel()'),
     ];
     $form['rss_channel']['copyright'] = [
       '#type' => 'textfield',
-      '#title' => t('Copyright'),
+      '#title' => $this->t('Copyright'),
       '#description' => t('Copyright notice for content in the channel.'),
       '#default_value' => $rss_channel['copyright'],
     ];
     $form['rss_channel']['managingEditor'] = [
       '#type' => 'textfield',
-      '#title' => t('Managing editor'),
-      '#description' => t('Email address for person responsible for editorial content.'),
+      '#title' => $this->t('Managing editor'),
+      '#description' => $this->t('Email address for person responsible for editorial content.'),
       '#default_value' => $rss_channel['managingEditor'] ? $rss_channel['managingEditor'] : '',
     ];
     $form['rss_channel']['webMaster'] = [
       '#type' => 'textfield',
-      '#title' => t('Webmaster'),
-      '#description' => t('Email address for person responsible for technical issues relating to channel.'),
+      '#title' => $this->t('Webmaster'),
+      '#description' => $this->t('Email address for person responsible for technical issues relating to channel.'),
       '#default_value' => $rss_channel['webMaster'] ? $rss_channel['webMaster'] : '',
     ];
     $form['buttons']['submit'] = [
       '#type' => 'submit',
-      '#value' => t('Save'),
+      '#value' => $this->t('Save'),
       '#weight' => 50,
-    ];
-    $form['buttons']['reset'] = [
-      '#type' => 'submit',
-      '#value' => t('Reset to defaults'),
-      '#weight' => 51,
     ];
 
     return $form;
   }
 
-  public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = \Drupal::configFactory()->getEditable('islandora_solr_config.settings');
+    $rss_item = $form_state->getValue('rss_item');
+    $rss_channel = $form_state->getValue('rss_channel');
 
-    // On save.
-    if ($form_state->get(['clicked_button', '#value']) == t('Save')) {
-
-      // Get values.
-      $rss_item = $form_state->getValue(['rss_item']);
-      $rss_channel = $form_state->getValue(['rss_channel']);
-
-      // Set variable.
-      \Drupal::configFactory()->getEditable('islandora_solr_config.settings')->set('islandora_solr_config_rss_item', $rss_item)->save();
-      \Drupal::configFactory()->getEditable('islandora_solr_config.settings')->set('islandora_solr_config_rss_channel', $rss_channel)->save();
-    }
-
-    // On reset.
-    if ($form_state->get(['clicked_button', '#value']) == t('Reset to defaults')) {
-      \Drupal::config('islandora_solr_config.settings')->clear('islandora_solr_config_rss_item')->save();
-      \Drupal::config('islandora_solr_config.settings')->clear('islandora_solr_config_rss_channel')->save();
-    }
+    // Set variable.
+    $config->set('islandora_solr_config_rss_item', $rss_item);
+    $config->set('islandora_solr_config_rss_channel', $rss_channel);
+    $config->save();
   }
 
 }
--- islandora_solr_config/src/Form/IslandoraSolrConfigAdminTableProfileSettings.php
+++ islandora_solr_config/src/Form/IslandoraSolrConfigAdminTableProfileSettings.php
@@ -1,15 +1,9 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\islandora_solr_config\Form\IslandoraSolrConfigAdminTableProfileSettings.
- */
-
 namespace Drupal\islandora_solr_config\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element;
 
 /**
  * Class for the table profile admin settings form.
@@ -23,53 +17,49 @@ class IslandoraSolrConfigAdminTableProfileSettings extends FormBase {
     return 'islandora_solr_config_admin_table_profile_settings';
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-
-    // @FIXME
-// // @FIXME
-// // This looks like another module's variable. You'll need to rewrite this call
-// // to ensure that it uses the correct configuration object.
-// $form['options']['islandora_solr_table_profile_settings'] = array(
-//     '#type' => 'fieldset',
-//     '#title' => t('General Table Settings'),
-//     '#collapsible' => TRUE,
-//     '#collapsed' => FALSE,
-//     'islandora_solr_table_profile_display_row_no' => array(
-//       '#type' => 'checkbox',
-//       '#title' => t('Display Row Numbers?'),
-//       '#default_value' => variable_get('islandora_solr_table_profile_display_row_no', 1),
-//       '#description' => t('Should row numbers be rendered as a column in the results table?'),
-//     ),
-//     'islandora_solr_table_profile_table_class' => array(
-//       '#type' => 'textfield',
-//       '#title' => t('Table Class'),
-//       '#default_value' => variable_get('islandora_solr_table_profile_table_class', ''),
-//       '#description' => t('A class string to set for the table element, if any.'),
-//     ),
-//   );
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['options']['islandora_solr_table_profile_settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('General Table Settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => FALSE,
+      'islandora_solr_table_profile_display_row_no' => [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Display Row Numbers?'),
+        '#default_value' => \Drupal::config('islandora_solr_config.settings')->get('islandora_solr_table_profile_display_row_no'),
+        '#description' => $this->t('Should row numbers be rendered as a column in the results table?'),
+      ],
+      'islandora_solr_table_profile_table_class' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Table Class'),
+        '#default_value' => \Drupal::config('islandora_solr_config.settings')->get('islandora_solr_table_profile_table_class'),
+        '#description' => $this->t('A class string to set for the table element, if any.'),
+      ],
+    ];
 
     $form['buttons']['submit'] = [
       '#type' => 'submit',
-      '#value' => t('Save'),
+      '#value' => $this->t('Save'),
     ];
 
     return $form;
   }
 
-  public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    // @FIXME
-// // @FIXME
-// // This looks like another module's variable. You'll need to rewrite this call
-// // to ensure that it uses the correct configuration object.
-// variable_set('islandora_solr_table_profile_display_row_no', $form_state['values']['islandora_solr_table_profile_display_row_no']);
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = \Drupal::configFactory()->getEditable('islandora_solr_config.settings');
 
-    // @FIXME
-// // @FIXME
-// // This looks like another module's variable. You'll need to rewrite this call
-// // to ensure that it uses the correct configuration object.
-// variable_set('islandora_solr_table_profile_table_class', $form_state['values']['islandora_solr_table_profile_table_class']);
+    $config->set(
+      'islandora_solr_table_profile_display_row_no',
+      $form_state->getValue('islandora_solr_table_profile_display_row_no')
+    );
+    $config->set(
+      'islandora_solr_table_profile_table_class',
+      $form_state->getValue('islandora_solr_table_profile_table_class')
+    );
 
-    drupal_set_message(t('The Solr table profile configuration options have been saved.'));
+    $config->save();
+
+    drupal_set_message($this->t('The Solr table profile configuration options have been saved.'));
   }
 
 }
