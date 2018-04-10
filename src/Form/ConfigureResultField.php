a58c46d5241be330ca331648df60a344d7dcdf2b
Adam Vessey <adam-vessey@users.noreply.github.com>
2018-04-10 13:14:16 -0300

    Coding standards.

--- src/Form/ConfigureResultField.php
+++ src/Form/ConfigureResultField.php
@@ -77,12 +77,12 @@ class ConfigureResultField extends FormBase {
       $form['options']['snippet']['#disabled'] = TRUE;
     }
 
-    $form['options']['replace_pid_with_label'] = array(
+    $form['options']['replace_pid_with_label'] = [
       '#type' => 'checkbox',
-      '#title' => t('Replace PID with Object Label'),
+      '#title' => $this->t('Replace PID with Object Label'),
       '#default_value' => isset($values['replace_pid_with_label']) ? $values['replace_pid_with_label'] : FALSE,
-      '#description' => t("Replace a PID (islandora:foo) or a URI (info:fedora/islandora:foo) with that object's label. Will only work with non-tokenized Solr fields (full literal strings)."),
-    );
+      '#description' => $this->t("Replace a PID (islandora:foo) or a URI (info:fedora/islandora:foo) with that object's label. Will only work with non-tokenized Solr fields (full literal strings)."),
+    ];
 
     if (islandora_solr_is_date_field($solr_field)) {
       $form['options']['date_format'] = [
